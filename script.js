a093a8dc7bdd3bb7924870584c20ade8b9bb5188
BenjaminGonzalez20 <jb59336876@gmail.com>
2026-04-24 00:44:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openCatalog(name) {
+  document.getElementById('catalog-' + name).style.display = 'flex';
+}
+
+function closeCatalog(name) {
+  document.getElementById('catalog-' + name).style.display = 'none';
+}
